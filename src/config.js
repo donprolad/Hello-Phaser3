315b4f188f21cbac32d770e016aadaf681fd0891
opstech23 <bbody8215@gmail.com>
2019-01-31 22:18:41 +0200

    added config, preload and create to index.html

---
+++ src/config.js
@@ -0,0 +1,19 @@
+import Phaser from 'phaser'
+
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 200 }
+    }
+  },
+  scene: {
+    preload: preload,
+    create: create
+  }
+}
+
+export default config
